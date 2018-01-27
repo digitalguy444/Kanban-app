d0aee7b541ef7ce5960875a26b648389f96b3620
Garrett <garrett.cambre@gmail.com>
2018-01-27 15:54:11 -0500

    added button in app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
 import Notes from './Notes';
 
-export default () => <Notes />;
+{/*export default () => <Notes />;*/}
+
+export default () => (
+<div>
+  <button onClick={() => console.log('add note')}>+
+  </button>
+  <Notes />
+</div>
+
+);
