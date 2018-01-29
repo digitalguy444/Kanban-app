dd5f171e8d286be84002080233592842adf4b1f1
Garrett <garrett.cambre@gmail.com>
2018-01-29 15:25:12 -0500

    routed notes array to app, notes just has the react element in it now

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+
+import uuid from 'uuid';
 import Notes from './Notes';
 
-{/*export default () => <Notes />;*/}
+const notes =[  //this is an array of notes as a frame for when real notes get in here
+{
+ id: uuid.v4(),
+  task:'learn react'
+},
+{
+  id: uuid.v4(),
+  task: 'do laundry'
+}
+
+];
+
 
 export default () => (
 <div>
   <button onClick={() => console.log('add note')}>+
   </button>
-  <Notes />
+  <Notes notes= {notes}/>
 </div>
 
 );
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
 
-import uuid from 'uuid';
 
-const notes = [
-	{
-		id: uuid.v4(), //calls on the uuid, a standard with a high probablility of uniqueness
-		task: 'Learn React' //this is going to have to be changed to a class or function for dynamic ability
-	},
-	{
-		id: uuid.v4(),
-		task: 'Do Laundry'
-	}
-];
-
-export default () => (
+export default ({notes}) => (
 <ul> {notes.map(note =>
 <li key={note.id}>{note.task}</li>
 
