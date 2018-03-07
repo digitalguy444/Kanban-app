3d312a60cb089dfbc17e1e5c2bd94bd4aa60fd3b
Garrett <garrett.cambre@gmail.com>
2018-03-07 12:24:09 -0500

    added alt file

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -8,15 +8,18 @@ export default ({
 }) =>(
       <ul className="notes">{notes.map(({id, editing, task}) =>
         <li key={id}>
-          <Note className="note" onClick={onNoteClick.bind(null, id)}>
 
-              <Editable className= "editable"
-                editing={editing}
-                value={task}
-                onEdit={onEdit.bind(null, id)} />
+          <Note
+          className="note"
+          onClick={onNoteClick.bind(null, id)}>
+                <Editable 
+                  className= "editable"
+                  editing={editing}
+                  value={task}
+                  onEdit={onEdit.bind(null, id)} />
 
-              <button className="delete"
-              onClick={onDelete.bind(null, id)}>x</button>
+                <button className="delete"
+                onClick={onDelete.bind(null, id)}>x</button>
           </Note>
         </li>
       )}</ul>
