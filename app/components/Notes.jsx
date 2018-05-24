3a7d12ee7da38f8f25c8b78f1cdce510eceee0ed
Garrett <garrett.cambre@gmail.com>
2018-05-24 10:50:41 -0400

    added drag and frop functionality though note and lanestore and added css rule to allow for better user visualization of where note will be dropped

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Note from './Note';
 import Editable from './Editable';
 
+import LaneActions from '../actions/LaneActions';
+
 export default ({
   notes,
   onNoteClick=() => {}, onEdit=() => {}, onDelete=() =>{}
@@ -10,8 +12,10 @@ export default ({
         <li key={id}>
 
           <Note
-          className="note"
-          onClick={onNoteClick.bind(null, id)}>
+          className="note" id={id}
+          onClick={onNoteClick.bind(null, id)}
+          onMove={LaneActions.move}>
+
                 <Editable
                   className= "editable"
                   editing={editing}
